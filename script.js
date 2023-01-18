78c50e0fec644a5eab3906b0e05ad897b4fb1bfa
Hubert <hubert.kozlowski.1998@gmail.com>
2023-01-18 23:21:30 +0100

    added red theme

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ let timer
 let timerDisplay
 let timeDecrease = 1000
 let count = timeDecrease/10
+const cssVariables = document.querySelector(':root')
+const colors = {
+    font: "--FONT-COLOR",
+    fontAdditional: "--FONT-COLOR-ADDITIONAL",
+    bgDark: "--BG-COLOR-DARK",
+    bgLight: "--BG-COLOR-LIGHT"
+}
+const themeRed = {
+    font: "#F7D6D0",
+    fontAdditional: "#F2C5E0",
+    bgDark: "#610b0b",
+    bgLight: "#FB6090"
+}
+const themeBlue = {
+    font: "#B4F5F0",
+    fontAdditional: "#2CEEF0",
+    bgDark: "#041F60",
+    bgLight: "#0476D0"
+}
+
+function changeTheme(theme) {
+    cssVariables.style.setProperty(colors.font, theme.font)
+    cssVariables.style.setProperty(colors.fontAdditional, theme.fontAdditional)
+    cssVariables.style.setProperty(colors.bgDark, theme.bgDark)
+    cssVariables.style.setProperty(colors.bgLight, theme.bgLight)
+}
 
 function randomNum(parentWidth, parentHeight, btnWidth, btnHeight) {
     return {
@@ -54,5 +80,11 @@ btn.addEventListener("click", () => {
     } else {
         btn.disabled = true
         btn.innerText = "END"
+    }
+
+    if (points % 2 === 0) {
+        changeTheme(themeBlue)
+    } else {
+        changeTheme(themeRed)
     }
 })
